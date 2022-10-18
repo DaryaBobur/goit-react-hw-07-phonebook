8d42587f32ca48c6bbf9030f4720d5a3a4038580
Darya <daryabobur@gmail.com>
2022-10-18 21:17:18 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,19 @@
 import ContactForm from '../ContactsForm/ContactsForm';
 import Filter from '../Filter/Filter';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Toaster } from 'react-hot-toast';
 import { ContainerApp, Title, Subtitle } from './AppStyled';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+const data = useSelector(state=>state.contacts.items) 
+console.log(data)
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <ContainerApp>
@@ -16,10 +24,10 @@ const App = () => {
 
       <Filter />
 
-      <ToastContainer 
-        autoClose={3000} 
-        theme={'colored'}
-      />
+      <Toaster
+  position="top-right"
+  reverseOrder={false}
+/>
 
     </ContainerApp>
   )
