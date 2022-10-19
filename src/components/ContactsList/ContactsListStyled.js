5de93f22b4f7ca89bd0d8f001eb572a558ee939b
Darya <daryabobur@gmail.com>
2022-10-19 19:46:34 +0300

    Update ContactsListStyled.js

--- src/components/ContactsList/ContactsListStyled.js
+++ src/components/ContactsList/ContactsListStyled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Contacts = styled.ul`
+padding-left: 0;
 
 li {
     display: flex;
@@ -11,16 +12,25 @@ li {
 }
 
 p {
+    display: flex;
+    align-items: center;
     font-size: 18px;
     font-weight: 500;
+
+    svg {
+        width: 20px;
+        height: 20px;
+        margin-right: 5px;
+        fill: #2a2a2a;
+    }
 }
 
 button {
     margin-left: 10px;
 
-    display: inline-block;
+    display: flex;
     border: 0;
-    padding: 2px 2px;
+    padding: 8px 8px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
     border-radius: 2px;
     color: #2a2a2a;
@@ -29,12 +39,12 @@ button {
     font-size: 12px;
     line-height: 1.88;
     letter-spacing: 0.06em;
-    background-color: #e7e5e5;
+    background-color: #cad7e3;
     cursor: pointer;
     transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
    
     :hover, :focus {
-        background-color: #d5d1d1;
+        background-color: #a5bacd;
         color: #544d4d;
     }
 }
