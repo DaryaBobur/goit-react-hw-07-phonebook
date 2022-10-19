113ebff2970890e4cee507dbf4dbff811aa53ed9
Darya <daryabobur@gmail.com>
2022-10-19 19:46:28 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,34 +1,33 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { useDispatch } from 'react-redux';
 import { Contacts } from './ContactsListStyled';
 import { deleteContact } from '../../redux/operations'
-import { BsFillTrashFill } from "react-icons/bs";
-import { FcCellPhone, FcMinus} from "react-icons/fc";
+import { BsFillTrashFill, BsPersonCircle } from "react-icons/bs";
+
 const ContactsList = ({ contacts }) => {
    const dispatch = useDispatch();
-   console.log(contacts)
    return (  
       <Contacts>
-          {contacts.map(({ name, phone, id }) => ( 
-         <li key={nanoid()}>
-               <p><FcCellPhone/> {name}<FcMinus/>{phone}</p>
-               <button type='button' onClick={()=> dispatch(deleteContact(id))}><BsFillTrashFill/></button>
+         {contacts.map(({name, phone, id}) => ( 
+            <li key={nanoid()}>
+               <p><BsPersonCircle/> {name} : {phone}</p>
+               <button type='button' onClick={()=> dispatch(deleteContact(id))}>
+                  <BsFillTrashFill/>
+               </button>
             </li>
-            )
-            )
-         }
+         ))}
       </Contacts>
    )
 }
-// onClick={()=> dispatch(removeContact(id))}
-// ContactsList.propTypes = {
-//    removeContact: PropTypes.func,
-//    contacts: PropTypes.arrayOf(PropTypes.shape({
-//       name: PropTypes.string.isRequired,
-//       number: PropTypes.string.isRequired,
-//       id: PropTypes.string.isRequired,
-//    }))
-// }
+
+ContactsList.propTypes = {
+   removeContact: PropTypes.func,
+   contacts: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      phone: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+   }))
+}
 
 export default ContactsList;
